c516aabbacf0a139548e09b0d2dd33d1e6091cf4
liam_davide_bryant_siciliano <liambryant@outlook.it>
2021-12-02 10:51:00 +0100

    Created r_id_body and coord_body subclasses to avoid rewriting same code over and over

--- iDelivery_bot_logic/src/srv_monitor/Requests/Body/body_types.h
+++ iDelivery_bot_logic/src/srv_monitor/Requests/Body/body_types.h
@@ -36,6 +36,59 @@ public:
 
 };
 
+class r_id_body:public generic_body{
+private:
+    string _body_str;
+    int _robot_id;
+public:
+    r_id_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _robot_id(0){}
+    void parse();
+    void print(out_mode out){
+        switch(out){
+            case cerr_out:
+                cerr << _body_str << endl;
+                cerr << "Robot_id:\t" << _robot_id << endl;
+                break;
+            case cout_out:
+                cout << _body_str << endl;
+                cout << "Robot_id:\t" << _robot_id << endl;
+                break;
+            case log_out:
+                break;
+        }
+    }
+};
+
+class coord_body:public r_id_body{
+private:
+    string _body_str;
+    coordinates_3D _coordinates; // [x, y, z]
+    int _robot_id;
+public:
+    coord_body(string body_str_):r_id_body(body_str_),_body_str(body_str_),_robot_id(0), _coordinates({0.0,0.0,0.0}){}
+    void parse();
+    void print(out_mode out){
+        switch(out){
+            case cerr_out:
+                cerr << _body_str << endl;
+                cerr << "Robot_id:\t" << _robot_id << endl;
+                cerr << "X:\t\t" << _coordinates[0]<<endl;
+                cerr << "Y:\t\t" << _coordinates[1] <<endl;
+                cerr << "Z:\t\t" << _coordinates[2] <<endl;
+                break;
+            case cout_out:
+                cout << _body_str << endl;
+                cout << "Robot_id:\t" << _robot_id << endl;
+                cout << "X:\t\t" << _coordinates.front()<<endl;
+                cout << "Y:\t\t" << _coordinates.front() <<endl;
+                cout << "Z:\t\t" << _coordinates.front() <<endl;
+                break;
+            case log_out:
+                break;
+        }
+    }
+};
+
 class login_body:public generic_body{
 private:
     string _body_str;
@@ -64,181 +117,44 @@ public:
 
 };
 
-class call_body:public generic_body{
-private:
-    string _body_str;
-    coordinates_3D _coordinates; // [x, y, z]
-    int _robot_id;
+class call_body:public coord_body{
 public:
-    call_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _coordinates({0.0,0.0,0.0}), _robot_id(0){}; 
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                cerr << "X:\t\t" << _coordinates[0]<<endl;
-                cerr << "Y:\t\t" << _coordinates[1] <<endl;
-                cerr << "Z:\t\t" << _coordinates[2] <<endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                cout << "X:\t\t" << _coordinates.front()<<endl;
-                cout << "Y:\t\t" << _coordinates.front() <<endl;
-                cout << "Z:\t\t" << _coordinates.front() <<endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
-
+    call_body(string body_str_):coord_body(body_str_){}; 
 };
 
-class priority_call_body:public call_body{
-private:
-    string _body_str;
-    coordinates_3D _coordinates; // [x, y, z]
-    int _robot_id;
+class priority_call_body:public coord_body{
 public:
-    priority_call_body(string body_str_):call_body(body_str_),_body_str(body_str_){};
-    
+    priority_call_body(string body_str_):coord_body(body_str_){};
 };
 
-class arrived_body:public generic_body{
+class arrived_body:public coord_body{
 private:
-    int _robot_id;
-    string _body_str;
-    coordinates_3D _coordinates;
 
 public:
-    arrived_body(string body_str_):generic_body(body_str_), _coordinates({0.0,0.0,0.0}),_body_str(body_str_), _robot_id(0){};
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                cerr << "X:\t\t" << _coordinates[0]<<endl;
-                cerr << "Y:\t\t" << _coordinates[1] <<endl;
-                cerr << "Z:\t\t" << _coordinates[2] <<endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                cout << "X:\t\t" << _coordinates.front()<<endl;
-                cout << "Y:\t\t" << _coordinates.front() <<endl;
-                cout << "Z:\t\t" << _coordinates.front() <<endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
-
+    arrived_body(string body_str_):coord_body(body_str_){};
 };
 
-class obj_sent_body:public generic_body{
-private:
-    string _body_str;
-    int _robot_id;
-
+class obj_sent_body:public r_id_body{
 public:
-    obj_sent_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _robot_id(0){};
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
-
+    obj_sent_body(string body_str_):r_id_body(body_str_){};
 };
 
-class obj_rcvd_body:public generic_body{
-private:
-    string _body_str;
-    int _robot_id;
-
+class obj_rcvd_body:public r_id_body{
 public:
-    obj_rcvd_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _robot_id(0){};
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
-
+    obj_rcvd_body(string body_str_):r_id_body(body_str_){};
 };
 
-class cancel_body:public generic_body{
-private:
-    string _body_str;
-    int _robot_id;
-
+class cancel_body:public r_id_body{
 public:
-    cancel_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _robot_id(0){};
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
+    cancel_body(string body_str_):r_id_body(body_str_){};
 };
 
-class timeout_body:public generic_body{
-private:
-    string _body_str;
-    int _robot_id;
-
+class timeout_body:public r_id_body{
 public:
-    timeout_body(string body_str_):generic_body(body_str_),_body_str(body_str_), _robot_id(0){};
-    void parse();
-    void print(out_mode out){
-        switch(out){
-            case cerr_out:
-                cerr << _body_str << endl;
-                cerr << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case cout_out:
-                cout << _body_str << endl;
-                cout << "Robot_id:\t" << _robot_id << endl;
-                break;
-            case log_out:
-                break;
-        }
-    }
+    timeout_body(string body_str_):r_id_body(body_str_){};
 };
 
 class invalid_body:public generic_body{
-private:
-    string _body_str;
-
 public:
     invalid_body(string body_str_):generic_body(body_str_){};
 
