f6173c7bda1e60217d41daeb04b32a9c5eb2456a
liam_davide_bryant_siciliano <liambryant@outlook.it>
2021-12-14 17:46:07 +0100

    Finally managed to start everything up

--- logic/src/delivery/src/main.cpp
+++ logic/src/delivery/src/main.cpp
@@ -1,25 +1,52 @@
 #include "srv_monitor_lib/message_creator.h"
 #include "srv_monitor_lib/srv_monitor.h"
 
+#include <ros/ros.h>
 
-int main(int argc, char const *argv[]){
+#include <unistd.h>
+
+using namespace ros;
+
+int main(int argc, char **argv){
     
-    req_parser parser;
+    ros::init(argc,argv,"delivery");
+    ros::NodeHandle n; 
+
+    req_parser parser; string login_str;
     srv_monitor monitor = srv_monitor();
-    string login_str;
 
-    while(1){
-        login_str = monitor.get_msg();
-        msg_type type = parser.get_msg_type(login_str);
-        string body = parser.get_msg_body(login_str);
-        int req_no = parser.get_req_no(login_str);
+    ros::Rate loop_rate(10);
+    int count = 0;
 
-        //late binding for body
-        req login_request = req(req_no, body, type, parser.cvt_msg_type_toString(type));
-        login_request.print_metadata(cerr);
-        login_request.act();
+    pid_t monitor_pid = fork();
+    if(monitor_pid<0){ perror("fork");exit(EXIT_FAILURE);}
+
+    //Monitor process
+    if (monitor_pid > 0){
+        while(ros::ok()){
+            login_str = monitor.get_msg();
+            msg_type type = parser.get_msg_type(login_str);
+            string body = parser.get_msg_body(login_str);
+            int req_no = parser.get_req_no(login_str);
+
+            //late binding for body
+            req login_request = req(req_no, body, type, parser.cvt_msg_type_toString(type));
+            login_request.print_metadata(cerr);
+            login_request.act();
+        }
+
+    } else {
+    //Ros process
+        while(ros::ok()){
+
+            std::cerr << "Running for: "<< count <<std::endl;
+            ros::spinOnce();
+            loop_rate.sleep();
+            count++;
+            
+        }
     }
 
-
-    return 0;
+    return EXIT_SUCCESS;
 }
+
