3ea57af4f23107b4642901dd4d322152f6c89201
Liam Bryant <liambryant@outlook.it>
2022-02-25 14:29:19 +0100

    Added a file to write a test

---
+++ srv_node/test/osx/test_res.js
@@ -0,0 +1,8 @@
+const { it } = require("mocha");
+const mocha = require("mocha");
+
+describe("Res Testing",function () {
+        it("Sending test res msg", function () {
+            
+        });
+    });
